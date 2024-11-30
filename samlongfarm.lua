7b54e7c930db8efa489f6ee1c7e6b0abd9af4ff2
petinjusemarang <vebryanzakaaulia@icloud.com>
2024-11-30 14:32:35 +0700

    Update samlongfarm.lua

--- samlongfarm.lua
+++ samlongfarm.lua
@@ -87,9 +87,14 @@ end
 jokiUangButton.MouseButton1Click:Connect(function()
     hideGUI() -- Menyembunyikan GUI
     -- Script khusus untuk Joki Uang
-getgenv().autoFarmValue = true
-script_key="AQrlbjFnTfgpymdjoFWKdKjuBfUDzIuy";
-    loadstring(game:HttpGet("https://getsades.net"))() -- Eksekusi script
+  script_key="AQrlbjFnTfgpymdjoFWKdKjuBfUDzIuy";
+    getgenv().beta = true -- Versi beta off
+    getgenv().autoFarmValue = true -- AutoFarm aktif
+    getgenv().alwaysRojod = true -- Always Rojod
+    getgenv().timeToTeleports = 45
+    getgenv().timeToShad = 1
+    getgenv().timeToRecall = 1
+    loadstring(game:HttpGet("https://getsades.net"))()
 end)
 
 -- Tombol Fungsi Joki Minigame
